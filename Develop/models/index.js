008bd221cb562c3374ad387d3f543f4b54f44efa
kate <katy.madden99@gmail.com>
2021-03-17 08:29:33 +0000

    Added in foreignKeys to fix bug

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -19,15 +19,18 @@ Category.hasMany(Product, {
 
 // Products belongToMany Tags (through ProductTag)
 
+
 Product.belongsToMany(Tag, {
-  through: ProductTag
-})
+  through: ProductTag,
+  foreignKey: 'product_id'
+});
 
 // Tags belongToMany Products (through ProductTag)
 
 Tag.belongsToMany(Product, {
-  through: ProductTag
-})
+  through: ProductTag,
+  foreignKey: 'tag_id'
+});
 
 module.exports = {
   Product,
